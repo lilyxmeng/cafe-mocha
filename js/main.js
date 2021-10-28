369008d8e2c25c2abd5d78d4f9d074fd90bf276b
Lily Meng <lilyxmeng13@gmail.com>
2021-10-28 18:15:55 -0400

    nav bar

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// burger dropdown
+function changeBurger(x) {
+    x.classList.toggle("change");
+    var navLinksMobile = document.getElementById("navLinksMobile");
+    navLinksMobile.classList.toggle("grid");
+}
